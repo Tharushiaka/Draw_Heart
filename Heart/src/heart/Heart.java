a272b4d2951695324e02e88ced0c602052037e84
Tharushika Ashani <69831766+Tharushiaka@users.noreply.github.com>
2021-10-25 15:11:05 +0530

    New Commit

---
+++ Heart/src/heart/Heart.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heart;
+
+/**
+ *
+ * @author ittr
+ */
+public class Heart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+  { 
+     for(int i=1; i<=2; i++)
+     {
+         for(int k=1; k<=2-i; k++)
+         {
+             System.out.print(" ");
+         } 
+         for(int j=1; j<=2*i; j++)
+         {
+             System.out.print("*");
+         } 
+         for(int k=1; k<=2*(2-i+1)-1; k++)
+         {
+             System.out.print(" ");
+         } 
+         for(int j=1; j<=2*i; j++)
+         {
+             System.out.print("*");
+         } 
+         System.out.println();
+     }
+     for(int i=1; i<=5; i++)
+     {
+         for(int k=1; k<=i-1; k++)
+         {
+             System.out.print(" ");
+         } 
+         for(int j=1; j<=2*(5-i+1)-1; j++)
+         {
+             System.out.print("*");
+         } 
+         System.out.println();
+     }
+  } 
+}
+
+
+    }
+ 
